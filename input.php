5330f3edabd353f90cede9562e085354fd5cfd9a
RyunosukeFunasako <e205732@ie.u-ryukyu.ac.jp>
2023-10-28 09:52:24 +0900

    add: XSS対策の追加

--- input.php
+++ input.php
@@ -6,6 +6,10 @@ if(!empty($_POST['btn_confirm'])){
 if(!empty($_POST['btn_submit'])){
     $pageFlag = 2;
 }
+
+function h($str){
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
 ?>
 
 <!DOCTYPE html>
@@ -18,10 +22,10 @@ if(!empty($_POST['btn_submit'])){
     <?php if($pageFlag === 0) : ?>
         <form method="POST" action="input.php">
             氏名
-            <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo $_POST['your_name'] ;} ?>">
+            <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']) ;} ?>">
             <br>
             メールアドレス
-            <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo $_POST['email'] ;} ?>">
+            <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']) ;} ?>">
             <br>
             <input type="submit" name="btn_confirm" value="確認する">
         </form>
@@ -30,15 +34,15 @@ if(!empty($_POST['btn_submit'])){
     <?php if($pageFlag === 1) : ?>
         <form method="POST" action="input.php">
             氏名
-            <?php echo $_POST['your_name'] ;?>
+            <?php echo h($_POST['your_name']) ;?>
             <br>
             メールアドレス
-            <?php echo $_POST['email'] ;?>
+            <?php echo h($_POST['email']) ;?>
             <br>
             <input type="submit" name="back" value="戻る">
             <input type="submit" name="btn_submit" value="送信する">
-            <input type="hidden" name="your_name" value="<?php echo $_POST['your_name'] ;?>">
-            <input type="hidden" name="email" value="<?php echo $_POST['email'] ;?>">
+            <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']) ;?>">
+            <input type="hidden" name="email" value="<?php echo h($_POST['email']) ;?>">
         </form>
     <?php endif; ?>
 
